a92b9335a407e0101f6447f0f174c377a2a0efd7
Saraaltaweel <s.bata97.st@gmail.com>
2021-07-25 15:20:47 +0300

    done

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const express = require ('express');
 const app = express();
 const cors = require ('cors');
 const http = require('http');
-cons server = http.createServer(app);
+const { Console } = require('console');
+const server = http.createServer(app);
+const PORT=process.env.PORT || 3000;
 const io = require ('socket.io')(http);
+
+
 
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'))
+
+io.on ('connection',socket=>{
+    let payload='welcome to my website';
+    io.emit('hello',payload);
+});
+
+server.listen(PORT,()=>{
+    console.log('server working in the port')
+});
